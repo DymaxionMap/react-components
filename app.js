2f590a415c00fb289c4d1af7bd84b3ecc4bda882
DymaxionMap <DymaxionMap@users.noreply.github.com>
2018-11-26 11:42:29 -0800

    Create GroceryList and two subcomponents

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// TODO
+const BlueCheese = () => (
+  <li>Blue cheese</li>  
+);
+
+const Mangoes = () => (
+  <li>Mangoes</li>
+);
+
+const GroceryList = () => (
+  <ul>
+    <BlueCheese />
+    <Mangoes />
+  </ul>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
